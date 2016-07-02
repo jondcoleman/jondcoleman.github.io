81a2aa7c495418de42eb3b9a3b9d6a86728d3511
Jonathan Coleman <jcoleman3307@gmail.com>
2016-07-02 15:10:41 -0400

    converting to es6

--- js/detectIE.js
+++ js/detectIE.js
@@ -1,20 +1,20 @@
 function detectIE() {
-  var ua = window.navigator.userAgent
+  const ua = window.navigator.userAgent
 
-  var msie = ua.indexOf('MSIE ')
+  const msie = ua.indexOf('MSIE ')
   if (msie > 0) {
     // IE 10 or older => return version number
     return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10)
   }
 
-  var trident = ua.indexOf('Trident/')
+  const trident = ua.indexOf('Trident/')
   if (trident > 0) {
     // IE 11 => return version number
-    var rv = ua.indexOf('rv:')
+    const rv = ua.indexOf('rv:')
     return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10)
   }
 
-  var edge = ua.indexOf('Edge/')
+  const edge = ua.indexOf('Edge/')
   if (edge > 11) return false
   if (edge > 0) {
     // Edge (IE 12+) => return version number
--- js/index.js
+++ js/index.js
@@ -1,46 +1,43 @@
-"use strict"
+/* global things detectIE*/
 
-function uniq(arr){
-  var newArr = []
-  arr.forEach((val) => newArr.indexOf(val) < 0 ? newArr.push(val) : null)
+function uniq(arr) {
+  const newArr = []
+  arr.forEach((val) => {
+    if (newArr.indexOf(val) < 0) newArr.push(val)
+  })
   return newArr
 }
 
-function main(){
-  var categories = uniq(things.map(function(thing) {
-    return thing.category
-  }))
-
-  var categoryThings = categories.map(function(cat) {
-    var thingsByCategory = things.filter(function(thing) {
-      return thing.category === cat
-    })
-    return { category: cat, things: thingsByCategory }
-  })
-
-  var thingsSection = document.getElementById('thing-section')
-
-  var thingsHtml = categoryThings.map(function(cat) {
-    var content = "<h3 class=\"section-header\">" + cat.category + " </h3>"
-    content += '<ol>'
-    var innerContent = ''
-
-    cat.things.forEach(function(thing) {
-      innerContent += "<li><a href=\"" + thing.link + "\" target='_blank'>" + thing.name + "</a> | <a href=\"" + thing.source + "\">source</a></li>"
-      innerContent += "<p>- " + thing.details + "</p>"
-    })
-
-    content += innerContent
-    content += '</ol>'
-
-    thingsSection.innerHTML += content
-  })
-}
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
+  const thingsSection = document.getElementById('thing-section')
   if (detectIE()) {
-    thingsSection.innerHTML = '<p class="ie-warning">Please use a modern browser to see my projects :)</p>'
+    thingsSection.innerHTML = `<p class="ie-warning">Please use a modern browser
+     to see my projects :)</p>`
   } else {
-    main()
+    const categories = uniq(things.map(thing => thing.category))
+
+    const categoryThings = categories.map(cat => {
+      const thingsByCategory = things.filter(thing => thing.category === cat)
+      return {
+        category: cat,
+        things: thingsByCategory
+      }
+    })
+
+    categoryThings.forEach(cat => {
+      let content = `<h3 class="section-header">${cat.category}</h3>`
+      content += '<ol>'
+      const innerContent = cat.things.map(thing => `<li>
+            <a href="${thing.link}" target='_blank'>${thing.name}</a> |
+            <a href="${thing.source}">source</a>
+          </li>
+          <p>- ${thing.details}</p>`
+      )
+
+      content += innerContent
+      content += '</ol>'
+
+      thingsSection.innerHTML += content
+    })
   }
 })
